e829fcda4bf921005433d5a882ef341d2aa8312e
Luan Luz <143976310+luan-luz@users.noreply.github.com>
2025-05-26 19:33:51 -0300

    Model att

--- AppJava/src/controller/LoginController.java
+++ AppJava/src/controller/LoginController.java
@@ -1,8 +1,13 @@
 package controller;
 
-
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpRequest.BodyPublishers;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -28,12 +33,13 @@ public class LoginController {
 
         if (username.isEmpty() || password.isEmpty()) {
             showAlert("Aviso", "Os campos não podem estar vazios", Alert.AlertType.WARNING);
-        } else if (username.equals("admin") && password.equals("admin")) {
-            String usuario = this.username.getText();
-            showAlert("Sucesso", "Login efetuado com sucesso", Alert.AlertType.INFORMATION);
-            sceneInterface(usuario);  // Chama a função para abrir a interface após o login bem-sucedido
         } else {
-            showAlert("Erro", "Usuário ou senha incorretos", Alert.AlertType.ERROR);
+            if (username.equals("admin") && password.equals("admin")) {
+                showAlert("Sucesso", "Login efeutado com sucesso", Alert.AlertType.INFORMATION);
+                sceneInterface(username);
+            }else {
+                showAlert("Erro", "Usuário ou senha incorretos", Alert.AlertType.ERROR);
+            }
         }
     }
 
@@ -47,38 +53,60 @@ public class LoginController {
 
     private void sceneInterface(String usuario) {
         try {
-            // Carregar o FXML da interface
-            /* Toda a configuração de resolução da interfacePatrimonio é feita aqui
-             quando instanciada a interface, ela recebe a largura e altura
-             da tela exceto a barra de tarefa */
-
             FXMLLoader interfaceLoader = new FXMLLoader(getClass().getResource("/view/InterfacePatrimonio.fxml"));
             Parent interfaceRoot = interfaceLoader.load();
             Stage interfaceStage = new Stage();
             Scene interfaceScene = new Scene(interfaceRoot);
-            // Obter o controller para inicializar com o nome de usuário
+
             InterfaceController interfaceController = interfaceLoader.getController();
             interfaceController.initializeInterface(usuario);
-            interfaceStage.setScene(interfaceScene);
-            // remover as bordas e os botões minimizar, expandir e fechar
-            interfaceStage.initStyle(StageStyle.UNDECORATED);
-            // Obter a área visível da tela
-            javafx.geometry.Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
-            // Definir a largura e altura da janela para ocupar toda a área visível da tela, exceto a barra de tarefas
-            interfaceStage.setWidth(screenBounds.getWidth());
-            interfaceStage.setHeight(screenBounds.getHeight());
-            interfaceStage.setX(screenBounds.getMinX());
-            interfaceStage.setY(screenBounds.getMinY());
 
-            // exibe a janela da interface
+            // Definir sem bordas
+            interfaceStage.initStyle(StageStyle.UNDECORATED);
+
+            // Obter apenas a área visível da tela (sem barra de tarefas)
+            Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
+
+            interfaceStage.setScene(interfaceScene);
+            interfaceStage.setX(visualBounds.getMinX());
+            interfaceStage.setY(visualBounds.getMinY());
+            interfaceStage.setWidth(visualBounds.getWidth());
+            interfaceStage.setHeight(visualBounds.getHeight());
+            interfaceStage.setResizable(false);
             interfaceStage.show();
 
-            // Fecha a janela de login
             Stage currentStage = (Stage) username.getScene().getWindow();
             currentStage.close();
         } catch (IOException e) {
-            e.printStackTrace();  // Exibe erro caso não consiga carregar o FXML
+            e.printStackTrace();
         }
     }
+
+    private String realizaLoginNaAPI(String username, String password) {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+
+            String json = String.format("{\"username\":\"%s\", \"password\":\"%s\"}", username, password);
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:8080/login/")) // Altere se sua API estiver em outra porta ou domínio
+                    .header("Content-Type", "application/json")
+                    .POST(BodyPublishers.ofString(json))
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200) {
+                return response.body(); // Token JWT
+            } else {
+                return null;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 
 }
---
+++ AppJava/src/controller/PreviewBagController.java
@@ -0,0 +1,18 @@
+package controller;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
+
+import java.io.IOException;
+
+
+public class PreviewBagController {
+
+
+}
